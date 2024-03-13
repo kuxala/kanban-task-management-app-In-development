39cd41013ab6798fc5f8c14fcbeaf13a68a53985
Roman Kukhalashvili <cyberdna12@gmail.com>
2024-03-13 22:49:54 +0400

    add: sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import Header from "./pages/Header";
 import Sidebar from "./pages/Sidebar";
-import { Poppins } from "next/font/google";
 
 export default function Home() {
   return (
     <>
-      {/* <Sidebar /> */}
       <Header />
+      <Sidebar />
     </>
   );
 }
--- app/pages/Sidebar.tsx
+++ app/pages/Sidebar.tsx
@@ -1,14 +1,21 @@
-import styled from "styled-components";
+import "../styles/Sidebar.css";
 
 export default function Sidebar() {
   return (
     <>
-      <div>
-        <img src="../../assets/logo-dark.svg" width="155px" />
-        <div>
-          <p>All Boards (number)</p>
-          <p>First one</p>
-          <p>third one</p>
+      <div className="sidebar">
+        <div className="sidebar__texts">
+          <p className="sidebar__header">All Boards (number)</p>
+          <div className="flex flex-col gap-2">
+            <p className="sidebar__boards">
+              <img src="/assets/icon-board.svg" />
+              First one
+            </p>
+            <p className="sidebar__boards">
+              <img src="/assets/icon-board.svg" />
+              Second one
+            </p>
+          </div>
         </div>
       </div>
     </>
