cdd9a1459bd8b45d7658fe2083d8d84724ab0c22
Roman Kukhalashvili <cyberdna12@gmail.com>
2024-03-14 11:26:34 +0400

    add: sidebar features

--- app/pages/Sidebar.tsx
+++ app/pages/Sidebar.tsx
@@ -7,15 +7,19 @@ export default function Sidebar() {
         <div className="sidebar__texts">
           <p className="sidebar__header">All Boards (number)</p>
           <div className="flex flex-col gap-2">
-            <p className="sidebar__boards">
+            <p className="sidebar__boards" tabIndex={0}>
               <img src="/assets/icon-board.svg" />
               First one
             </p>
-            <p className="sidebar__boards">
+            <p className="sidebar__boards" tabIndex={0}>
               <img src="/assets/icon-board.svg" />
               Second one
             </p>
           </div>
+          <p className="sidebar__add__board flex gap-2">
+            <img src="/assets/icon-board.svg" width="16px" height="16px" />+
+            Create New Board
+          </p>
         </div>
       </div>
     </>
